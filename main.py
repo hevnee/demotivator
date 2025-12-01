56e1e4efbc77a6c7d2e0e9a534797528d055a551
Serhii Belous <hevneebelous@gmail.com>
2025-12-01 17:32:00 +0100

    chore: initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import sys
+
+from PySide6.QtWidgets import QApplication
+
+from src import MainWindow
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    app.setStyle("Fusion")
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec())
